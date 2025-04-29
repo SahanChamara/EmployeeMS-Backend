7c1844bf28dcfc4da90af4925e0babe88fc24738
Sahan Chamara <sahanchamara456@gmail.com>
2025-04-29 11:03:11 +0530

    employee entity create and dto some validation add

--- src/main/java/org/icet/crm/entity/EmployeeEntity.java
+++ src/main/java/org/icet/crm/entity/EmployeeEntity.java
@@ -1,5 +1,6 @@
-package org.icet.crm.dto;
+package org.icet.crm.entity;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
@@ -7,6 +8,8 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.validator.constraints.UniqueElements;
 
 import java.time.LocalDate;
@@ -15,20 +18,27 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Employee {
+@Entity
+@Table(name = "employee")
+public class EmployeeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotEmpty
-    @Size(min = 2, max = 100, message = "user name should have at least 2 characters")
     private String name;
 
     @Email
     @UniqueElements
-    @NotEmpty
     private String email;
 
     @NotBlank
     private String  department;
+
+    @CreationTimestamp
     private LocalDateTime createAt;
+
+    @UpdateTimestamp
     private LocalDateTime updatedAt;
 }
